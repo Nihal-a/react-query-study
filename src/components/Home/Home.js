369daf1f82aba0fee944775e26bd3304e7515663
Nihal-a <nihalavulan1@gamil.com>
2021-11-13 14:22:34 +0530

    Post fetched and errror handled on fetching post

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,6 @@
+import { Container, Flex, Grid, Heading, Stack, Text } from '@chakra-ui/layout'
+import { Spinner } from '@chakra-ui/spinner'
+import { useToast } from '@chakra-ui/toast'
 import axios from 'axios'
 import React from 'react'
 import { useQuery } from 'react-query'
@@ -15,15 +18,29 @@ const fetchPost = async () => {
 
 function Home() {
 
-    const {data , isLoading , error , isError}  = useQuery("Posts" , fetchPost)
+    const {data , isLoading }  = useQuery("Posts" , fetchPost,{
+        onError : (error) => {
+            toast({status:"error" , title: error.message})
+        }
+    })
+    const toast = useToast()
 
-    console.log(data);
 
     return (
-        <div>
-            
-        </div>
+        <Container maxW="1300px" mt="4">
+            {isLoading ? <Grid placeItems="center" height="100vh"><Spinner /></Grid> : <>{data.data.map((post) => (
+                <Stack p="4" boxShadow="md" borderRadius="x1" border="1px solid #ccc" key={post.id} mb="4">
+                <Flex justify="space-between">
+                    <Text>UserId : {post.user_id}</Text>
+                    <Text>PostId : {post.id}</Text>
+                </Flex>
+                <Heading fontSize="2xl">{post.title}</Heading>
+                <Text>{post.body}</Text>
+                </Stack>
+            ))}</>}
+        </Container>
     )
 }
 
 export default Home
+
