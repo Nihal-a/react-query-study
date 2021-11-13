b4c02dcfa06fd7ce6528d6ac7d0d95b97457dc65
Nihal-a <nihalavulan1@gamil.com>
2021-11-13 13:58:21 +0530

    router setups done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { QueryClientProvider, QueryClient } from "react-query"
 import { ChakraProvider , Heading} from "@chakra-ui/react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import Home from  "./components/Home/Home"
 
 function App() {
     const queryClient = new QueryClient()
     return (
         <ChakraProvider>
             <QueryClientProvider client={queryClient}>
-                <Heading>hello </Heading>
+                <Router>
+                    <Switch>
+                        <Route exact>
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </Router>
             </QueryClientProvider>
         </ChakraProvider>
     )
