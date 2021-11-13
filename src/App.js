6d59eb5fb03cb0beff28d63901c6cbf0ad14728a
Nihal-a <nihalavulan1@gamil.com>
2021-11-13 13:10:40 +0530

    Chakra ui installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { QueryClientProvider, QueryClient } from "react-query"
+import { ChakraProvider , Heading} from "@chakra-ui/react"
 
 function App() {
+    const queryClient = new QueryClient()
     return (
-        <div>
-            hello
-        </div>
+        <ChakraProvider>
+            <QueryClientProvider client={queryClient}>
+                <Heading>hello </Heading>
+            </QueryClientProvider>
+        </ChakraProvider>
     )
 }
 
